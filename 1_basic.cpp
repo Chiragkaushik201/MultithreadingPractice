4d249fbe9e01716eaf2dbc9c9c6cf5f4a45937f0
Chiragkaushik201 <chirag@DESKTOP-6BLG18U.localdomain>
2025-09-21 15:25:28 +0530

    first file

--- 1_basic.cpp
+++ 1_basic.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+/*Default thread: Main thread()
+ * Under that multiple light weight process called threads are created. Thread helps in implemneting parallelism
+ *
+ * Ways to create thraed:
+ * Function pointer
+ * Lmabda function
+ * Functor 
+ * Member function
+ * Static member function
+ *
+ * NOTES:
+ *
+ *
+ * */
+
+
+#includei<iostream>
 
 int main()
 {
+
 
 	return 0;
 }
